87f6e0ea4ed24c1532ec872ebb649d4743dbabb6
TrujiDev <eetgrisales@gmail.com>
2023-12-17 11:16:34 -0500

    Add cron job to server startup

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,10 @@
+import { CronService } from './cron/cronService';
+
 export class ServerApp {
 	public static start() {
 		console.log('Server started');
+		CronService.createJob('*/5 * * * * *', () => {
+			console.log('Cron job running every 5 seconds');
+		});
 	}
 }
