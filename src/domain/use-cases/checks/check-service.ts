7fc9ba119b4ed9af59acdb6d3c364ed81ec2a8a0
TrujiDev <eetgrisales@gmail.com>
2023-12-17 14:09:55 -0500

    Refactor CheckService to include success and error callbacks

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -2,17 +2,27 @@ interface CheckServiceUseCase {
 	execute(url: string): Promise<boolean>;
 }
 
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 export class CheckService implements CheckServiceUseCase {
+    constructor(
+        private readonly successCallback: SuccessCallback,
+        private readonly errorCallback: ErrorCallback,
+    ) {}
+
 	public async execute(url: string): Promise<boolean> {
 		try {
 			const req = await fetch(url);
-			if (!req.ok) {
-				throw new Error(`Error on check service ${url}`);
+            if (!req.ok) {
+                const d = Date();
+				throw new Error(`Check service ${url} is not ok at ${d}`);
             }
-            console.log(`Check service ${url} is ok`);
+            this.successCallback();
 			return true;
         } catch (error) {
             console.error(`${error}`);
+            this.errorCallback('error');
 			return false;
 		}
 	}
