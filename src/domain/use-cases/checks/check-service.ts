009620c9c18c4b337ab4fe01f8a895bab6e87b98
TrujiDev <eetgrisales@gmail.com>
2023-12-18 16:58:56 -0500

    More descriptive error message

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase {
 	execute(url: string): Promise<boolean>;
 }
@@ -7,6 +10,7 @@ type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback,
     ) {}
@@ -18,11 +22,15 @@ export class CheckService implements CheckServiceUseCase {
                 const d = Date();
 				throw new Error(`Check service ${url} is not ok at ${d}`);
             }
+            const log = new LogEntity(`Check service ${url} is ok`, LogSeverityLevel.low);
+            this.logRepository.saveLog(log);
             this.successCallback();
 			return true;
         } catch (error) {
-            console.error(`${error}`);
-            this.errorCallback('error');
+            const errorMessage = `Check service ${url} is not ok. ${error}`;
+            const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+            this.logRepository.saveLog(log);
+            this.errorCallback(errorMessage);
 			return false;
 		}
 	}
