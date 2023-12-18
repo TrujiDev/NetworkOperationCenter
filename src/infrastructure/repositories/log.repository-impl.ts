9b3f4a493bf388312e39f70d088fde39efec5b5d
TrujiDev <eetgrisales@gmail.com>
2023-12-18 15:08:52 -0500

    Refactor log.repository-impl.ts

--- src/infrastructure/repositories/log.repository-impl.ts
+++ src/infrastructure/repositories/log.repository-impl.ts
@@ -1,19 +1,19 @@
-import { LogDatasource } from "../../domain/datasources/log.datasource";
-import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
-import { LogRepository } from "../../domain/repository/log.repository";
+import { LogDatasource } from '../../domain/datasources/log.datasource';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
+import { LogRepository } from '../../domain/repository/log.repository';
 
 export class LogRepositoryImpl implements LogRepository {
-    private readonly logDatasource: LogDatasource;
+	private readonly logDatasource: LogDatasource;
 
-    constructor(logDatasource: LogDatasource) {
-        this.logDatasource = logDatasource;
-    }
-    
-    async saveLog(log: LogEntity): Promise<void> {
-        return this.logDatasource.saveLog(log);
-    }
+	constructor(logDatasource: LogDatasource) {
+		this.logDatasource = logDatasource;
+	}
 
-    async getLogs(securityLevel: LogSeverityLevel): Promise<LogEntity[]> {
-        return this.logDatasource.getLogs(securityLevel);
-    }
+	async saveLog(log: LogEntity): Promise<void> {
+		return this.logDatasource.saveLog(log);
+	}
+
+	async getLogs(securityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+		return this.logDatasource.getLogs(securityLevel);
+	}
 }
