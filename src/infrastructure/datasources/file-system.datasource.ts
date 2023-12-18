8004f4ca0894b4b942109b3268be37d68f1f39d3
TrujiDev <eetgrisales@gmail.com>
2023-12-18 13:58:30 -0500

    Refactor log saving logic in FileSystemDatasource

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -4,13 +4,14 @@ import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 
 export class FileSystemDatasource implements LogDatasource {
 	private readonly logPath = 'logs/';
+	private readonly allLogsPath = 'logs/all-logs.log';
 	private readonly lowLogsPath = 'logs/logs-low.log';
 	private readonly mediumLogsPath = 'logs/logs-medium.log';
 	private readonly highLogsPath = 'logs/logs-high.log';
 
-    constructor() {
-        this.createLogsFile();
-    }
+	constructor() {
+		this.createLogsFile();
+	}
 
 	private createLogsFile = () => {
 		if (!fs.existsSync(this.logPath)) {
@@ -23,8 +24,24 @@ export class FileSystemDatasource implements LogDatasource {
 		});
 	};
 
-	saveLog(log: LogEntity): Promise<void> {
-		throw new Error('Method not implemented.');
+	async saveLog(log: LogEntity): Promise<void> {
+		const logAsJson = `${JSON.stringify(log)}\n`;
+
+		fs.appendFileSync(this.allLogsPath, logAsJson);
+
+		switch (log.level) {
+            case LogSeverityLevel.low:
+                fs.appendFileSync(this.lowLogsPath, logAsJson);
+                break;
+            case LogSeverityLevel.medium:
+                fs.appendFileSync(this.mediumLogsPath, logAsJson);
+                break;
+            case LogSeverityLevel.high:
+                fs.appendFileSync(this.highLogsPath, logAsJson);
+                break;
+            default:
+                break;
+        }
 	}
 	getLogs(securityLevel: LogSeverityLevel): Promise<LogEntity[]> {
 		throw new Error('Method not implemented.');
