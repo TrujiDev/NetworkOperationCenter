3557f08be029331a277029c4ad05a3b094ee1ed4
TrujiDev <eetgrisales@gmail.com>
2023-12-19 18:21:57 -0500

    Add email service and send email logs

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,11 @@
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository-impl';
 import { CronService } from './cron/cronService';
+import { EmailService } from './email/email.service';
+
+const emailService = new EmailService();
 
 const fileSystemLogRepository = new LogRepositoryImpl(
 	new FileSystemDatasource()
@@ -10,8 +14,19 @@ const fileSystemLogRepository = new LogRepositoryImpl(
 export class ServerApp {
 	public static start() {
 		console.log('Server started');
+
+		new SendEmailLogs(emailService, fileSystemLogRepository).execute([
+			'kutedokashi@gmail.com',
+			'eetgrisales@gmail.com',
+		]);
+
+		// emailService.sendEmailWithFileSystemLogs([
+		// 	'kutedokashi@gmail.com',
+		// 	'eetgrisales@gmail.com',
+		// ]);
+
 		CronService.createJob('*/10 * * * * *', () => {
-			const url = 'http://localhost:3000/'
+			const url = 'http://localhost:3000/';
 			new CheckService(
 				fileSystemLogRepository,
 				() => console.log('Service is ok'),
