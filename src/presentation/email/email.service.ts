ad9ff43e2ac3d8536c53f142f770bbb29ef858ee
TrujiDev <eetgrisales@gmail.com>
2023-12-19 11:55:24 -0500

    Refactor email service to support attachments

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,38 +1,73 @@
 import nodemailer from 'nodemailer';
 import { ENVS } from '../../config/plugins/env.plugin';
 
-interface sendEmailOptions { 
-    to: string;
-    subject: string;
-    htmlBody: string;
-    // attachments
+interface sendEmailOptions {
+	to: string | string[];
+	subject: string;
+	htmlBody: string;
+	attachments?: Attachment[];
+}
+
+interface Attachment {
+	filename: string;
+	path: string;
 }
 
 export class EmailService {
-    private transporter = nodemailer.createTransport({
-        service: ENVS.MAILER_SERVICE,
-        auth: {
-            user: ENVS.MAILER_EMAIL,
-            pass: ENVS.MAILER_SECRET_KEY,
-        }
-    });
+	private transporter = nodemailer.createTransport({
+		service: ENVS.MAILER_SERVICE,
+		auth: {
+			user: ENVS.MAILER_EMAIL,
+			pass: ENVS.MAILER_SECRET_KEY,
+		},
+	});
 
-    async sendEmail(options: sendEmailOptions): Promise<boolean> {
-        const { to, subject, htmlBody } = options;
-        
-        try {
-            const sentInformation = await this.transporter.sendMail({
-                from: ENVS.MAILER_EMAIL,
-                to,
-                subject,
-                html: htmlBody,
-            });
+	async sendEmail(options: sendEmailOptions): Promise<boolean> {
+		const { to, subject, htmlBody, attachments = [] } = options;
 
-            console.log(sentInformation);
+		try {
+			const sentInformation = await this.transporter.sendMail({
+				from: ENVS.MAILER_EMAIL,
+				to,
+				subject,
+				html: htmlBody,
+				attachments,
+			});
 
-            return true
-        } catch (error) {
-            return false
-        }
-    }
+			console.log(sentInformation);
+
+			return true;
+		} catch (error) {
+			return false;
+		}
+	}
+
+	async sendEmailWithFileSystemLogs(to: string | string[]) {
+		const subject = 'Logs de sistema - NOC';
+		const htmlBody = `
+            <h1>Logs de sistema - NOC</h1>
+            <p>Registro de todos los logs generados por el sistema</p>
+            <p>Fecha: ${new Date().toLocaleString()}</p>
+        `;
+		const attachments: Attachment[] = [
+			{
+				filename: 'all-logs.log',
+				path: './logs/all-logs.log',
+			},
+			{
+				filename: 'logs-low.log',
+				path: './logs/logs-low.log',
+			},
+			{
+				filename: 'logs-medium.log',
+				path: './logs/logs-medium.log',
+			},
+			{
+				filename: 'logs-high.log',
+				path: './logs/logs-high.log',
+			},
+		];
+
+		return this.sendEmail({ to, subject, htmlBody, attachments });
+	}
 }
