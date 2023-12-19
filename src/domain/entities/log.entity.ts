1307f474859b6c631ee611c18dde2885355c46e3
TrujiDev <eetgrisales@gmail.com>
2023-12-19 09:50:45 -0500

    Refactor LogEntity constructor and add origin property

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -1,24 +1,38 @@
 export enum LogSeverityLevel {
-    low = 'low',
-    medium = 'medium',
-    high = 'high',
+	low = 'low',
+	medium = 'medium',
+	high = 'high',
+}
+
+export interface LogEntityOptions {
+    level: LogSeverityLevel;
+    message: string;
+    createdAt?: Date;
+    origin: string;
 }
 
 export class LogEntity {
-    public level: LogSeverityLevel;
-    public message: string;
-    public createdAt: Date;
+	public level: LogSeverityLevel;
+	public message: string;
+	public createdAt: Date;
+	public origin: string;
 
-    constructor(message: string, level: LogSeverityLevel) {
-        this.message = message;
-        this.level = level;
-        this.createdAt = new Date();
-    }
+    constructor(options: LogEntityOptions) {
+        const { level, message, createdAt = new Date(), origin } = options;
+		this.message = message;
+		this.level = level;
+        this.createdAt = createdAt;
+        this.origin = origin;
+	}
 
-    static fromJson(json: string): LogEntity { 
-        const { message, level, createdAt } = JSON.parse(json);
-        const log = new LogEntity(message, level);
-        log.createdAt = new Date(createdAt);
-        return log;
-    }
+	static fromJson(json: string): LogEntity {
+		const { message, level, createdAt, origin } = JSON.parse(json);
+        const log = new LogEntity({
+            message,
+            level,
+            createdAt,
+            origin,
+        });
+		return log;
+	}
 }
