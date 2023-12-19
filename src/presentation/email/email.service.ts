55fd69dbe39ddfadc758295e1284d2efd0d5ea1c
TrujiDev <eetgrisales@gmail.com>
2023-12-19 12:33:59 -0500

    Creation of log when sending an email

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer';
 import { ENVS } from '../../config/plugins/env.plugin';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
+import { LogRepository } from '../../domain/repository/log.repository';
 
 interface sendEmailOptions {
 	to: string | string[];
@@ -22,6 +24,8 @@ export class EmailService {
 		},
 	});
 
+	constructor(private readonly logRepository: LogRepository) {}
+
 	async sendEmail(options: sendEmailOptions): Promise<boolean> {
 		const { to, subject, htmlBody, attachments = [] } = options;
 
@@ -36,8 +40,22 @@ export class EmailService {
 
 			console.log(sentInformation);
 
+			const log = new LogEntity({
+				level: LogSeverityLevel.low,
+				message: `Email sent to ${to}`,
+				origin: 'email.service.ts',
+			});
+			this.logRepository.saveLog(log);
+
 			return true;
 		} catch (error) {
+			const log = new LogEntity({
+				level: LogSeverityLevel.high,
+				message: `Error sending email to ${to}`,
+				origin: 'email.service.ts',
+			});
+			this.logRepository.saveLog(log);
+
 			return false;
 		}
 	}
