2f1f31a995a5e1ccb5ba78d7175c5f16d52df857
TrujiDev <eetgrisales@gmail.com>
2023-12-22 15:47:24 -0500

    Add LogModel to MongoDB import in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { ENVS } from './config/plugins/env.plugin';
-import { MongoDB } from './data/mongo';
+import { LogModel, MongoDB } from './data/mongo';
 import { ServerApp } from './presentation/server';
 
 (() => {
@@ -11,5 +11,17 @@ async function main() {
 		mongoURL: ENVS.MONGO_URL,
 		dbName: ENVS.MONGO_DB_NAME,
 	});
+
+	// const newLog = await LogModel.create({
+	// 	message: 'Test log message from app.ts',
+	// 	origin: 'app.ts',
+	// 	level: 'medium',
+	// });
+	// await newLog.save();
+	// console.log(newLog);
+
+	// const logs = await LogModel.find();
+	// console.log(logs);
+
 	ServerApp.start();
 }
