02b7f5269dd5d65383706a769578b39c31af53aa
TrujiDev <eetgrisales@gmail.com>
2023-12-22 13:34:58 -0500

    Add LogModel to handle log data

---
+++ src/data/mongo/models/log.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose';
+
+const logSchema = new Schema({
+    message: {
+        type: String,
+        required: true,
+    },
+    origin: {
+        type: String,
+    },
+    level: {
+        type: String,
+        enum: ['low', 'medium', 'high'],
+        default: 'low',
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+});
+
+export const LogModel = model('Log', logSchema);
